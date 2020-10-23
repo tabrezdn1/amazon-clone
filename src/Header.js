d9fa24aa7b7906a670d30088a41a4be4e2f8be9a
tabrezdn1 <tabrezdn1@gmail.com>
2020-10-23 23:35:17 +0530

    Update header

--- src/Header.js
+++ src/Header.js
@@ -1,40 +1,49 @@
-import React from 'react'
-import './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingCartRoundedIcon from '@material-ui/icons/ShoppingCartRounded';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 function Header() {
-    return (
-        <div className="header">
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon logo" />
-            <div className="header__search">
-                <input className="header__search__input" type="text" />
-                <SearchIcon className="header__search__icon" />
-            </div>
-            <div className="header__nav">
-                <div className="header__nav__option">
-                    <span className="header__nav__option__heading">Hello Guest</span>
-                    <span className="header__nav__option__subheading">Sign In</span>
+  return (
+    <div className="header">
 
-                </div>
-                <div className="header__nav__option">
-                    <span className="header__nav__option__heading">Returns</span>
-                    <span className="header__nav__option__subheading">& orders</span>
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
 
-                </div>
-                <div className="header__nav__option">
-                    <span className="header__nav__option__heading">Your</span>
-                    <span className="header__nav__option__subheading">Prime</span>
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
 
-                </div>
-                <div className="header__nav__option__cart">
-                    <ShoppingCartRoundedIcon />
-                    <span className="header__nav__option__cart__count">0</span>
+      <div className="header__nav">
 
-                </div>
-            </div>
-
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
-    )
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">
+            0
+          </span>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
